223b8e54b0944217140ec81d1c86dc7c266d64f0
Robin <schmidtrobin02@gmail.com>
2023-04-19 21:22:14 +0200

    Added Gson Dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module gui {
     requires javafx.fxml;
     requires javafx.media;
     requires org.apache.logging.log4j;
+    requires com.google.gson;
 
     opens org.example to javafx.fxml;
     exports org.example;
