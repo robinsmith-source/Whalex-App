91d2c7a4bdd12c53fc149127cb997325bfd5325b
Robin <schmidtrobin02@gmail.com>
2023-05-12 15:03:02 +0200

    Cleaned up project

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,10 +4,12 @@ module gui {
     requires javafx.media;
     requires org.apache.logging.log4j;
     requires com.google.gson;
+    requires java.desktop;
 
     opens org.example to javafx.fxml;
     opens org.example.controller to javafx.fxml;
     exports org.example;
     exports org.example.controller;
     exports org.example.exceptions;
+    exports org.example.media.interfaces;
 }
