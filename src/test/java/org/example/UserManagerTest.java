b6d10cc484af9f3077c28eb414fc83ebd88a8e83
Robin <schmidtrobin02@gmail.com>
2023-06-25 16:58:16 +0200

    Changes on UserManagerTest

--- src/test/java/org/example/UserManagerTest.java
+++ src/test/java/org/example/UserManagerTest.java
@@ -1,33 +1,46 @@
 package org.example;
 
+import org.example.media.SoundManager;
+import org.example.media.interfaces.ISound;
 import org.example.profile.User;
 import org.example.profile.UserManager;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
 
 public class UserManagerTest {
 
     final UserManager um = UserManager.getInstance();
 
-    User Marvin;
+    ISound sound;
 
-    //TODO: Remove and add a relative resource path to the Managers
-    @BeforeEach
-    public void setup() {
-
+    /**
+     * Positive Tests
+     */
+    @Test
+    public void testCreateUser() {
+        Assertions.assertDoesNotThrow(() -> um.createUser(null, "Marvin", "123", "123"));
+        User Marvin = um.getUserByName("Marvin");
+        Assertions.assertEquals(Marvin.getProfilePicture().toString(), (UserManager.getInstance().getPROFILE_PICTURES() + "\\default\\defaultUserProfilePicture.jpg"));
     }
 
     @Test
-    public void testCreateUser() {
+    public void createSound() {
         //positive test
         Assertions.assertDoesNotThrow(() -> um.createUser(null, "Marvin", "123", "123"));
-        Marvin = um.getUserByName("Marvin");
+        User Marvin = um.getUserByName("Marvin");
+        Assertions.assertDoesNotThrow(() -> SoundManager.getInstance().addSound(Marvin, "Pottwal Pop", new File("src/main/resources/data/sounds/example/72021005.wav")));
+        sound = SoundManager.getInstance().getAllSoundsByUser(Marvin).get(0);
+        Assertions.assertDoesNotThrow(() -> SoundManager.getInstance().deleteSoundByID(Marvin, sound.getSoundID()));
+    }
 
-        Assertions.assertEquals(um.getUserByName("Marvin"), Marvin);
-        Assertions.assertEquals(um.getUserById(Marvin.getUserID()), Marvin);
+    @Test
+    public void deleteSound() {
 
-        Assertions.assertEquals("src\\main\\resources\\data\\defaultImages\\users\\defaultUserProfilePicture.jpg", Marvin.getProfilePicture().getPath());
+    }
+
+    public void t() {
         //negative tests
         //Username null or empty
         Assertions.assertThrows(IllegalArgumentException.class, () -> um.createUser(null, "", "123", "123"));
@@ -46,6 +59,8 @@ public class UserManagerTest {
 
     @Test
     public void testLogin() {
+        Assertions.assertDoesNotThrow(() -> um.createUser(null, "Marvin", "123", "123"));
+        User Marvin = um.getUserByName("Marvin");
         Assertions.assertDoesNotThrow(() -> um.login("Marvin", "123"));
         Assertions.assertEquals(um.getActiveUser(), Marvin);
     }
@@ -53,7 +68,9 @@ public class UserManagerTest {
     @Test
     public void testDeleteUser() {
         //positive test
-        Assertions.assertDoesNotThrow(() -> um.deleteUser("123","123"));
+        Assertions.assertDoesNotThrow(() -> um.createUser(null, "Marvin", "123", "123"));
+        User Marvin = um.getUserByName("Marvin");
+        Assertions.assertDoesNotThrow(() -> um.deleteUser("123", "123"));
         Assertions.assertNull(um.getUserByName("Marvin"));
         Assertions.assertNull(um.getUserById(Marvin.getUserID()));
 
